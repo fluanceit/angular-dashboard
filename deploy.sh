9e5a7a5ea1d82d20a4857ea70d92f4edfc78cf05
Sébastien BARBIER <contact@sebastienbarbier.com>
2015-08-19 09:22:18 +0200

    Add automated push of documentation

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+github_user=`git remote -v | grep push | sed -e 's/^.*://g' -e 's/\/.*$//g'`
+if [ "$github_user" == "sebastienbarbier" ]; then
+    echo "NOT A FORK, so deploy here..."
+    echo github_user
+    cd build_docs
+    git init
+    git config user.name "Travis CI"
+    git config user.email "frontdev@fluance.net"
+    git add .
+    git commit -m "Deploy to GitHub Pages"
+    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+fi
