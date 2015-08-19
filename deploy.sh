373dd0b788aad07d2d39d6595e190b37ec6cbf9f
Sebastien BARBIER <contact@sebastienbarbier.com>
2015-08-19 14:01:00 +0200

    Add echo to see user value

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+echo "DEPLOY"
 github_user=`git remote -v | grep push | sed -e 's/^.*://g' -e 's/\/.*$//g'`
+echo ${github_user}
 if [ "$github_user" == "sebastienbarbier" ]; then
     cd build_docs
     git init
