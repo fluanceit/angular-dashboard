b9a4274717a54386dd53adf33271999c2c0f69f9
Sebastien BARBIER <contact@sebastienbarbier.com>
2015-10-29 10:08:27 +0100

    Add more fancy pictures in demo
    Just to please Dominik ;)

--- src/dashboard.directive.js
+++ src/dashboard.directive.js
@@ -2,75 +2,91 @@
  * Mycockpit Directive
  */
 (function() {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('dashboard')
-        .directive('dashboard', ['dashboardFactory', function(dashboardFactory) {
-            return {
-                restrict: 'E',
-                scope: {
-                    'id': '@',
-                    'width': '@',
-                    'columns': '@',
-                    'columnsMinWidth': '@'
-                },
-                templateUrl: 'src/dashboard.directive.html',
-                controller: ['$scope', function (scope) {
+	angular
+		.module('dashboard')
+		.directive('dashboard', ['dashboardFactory', function(dashboardFactory) {
 
-                    var currentWidth = document.getElementById(scope.id).offsetWidth;
-                    // If screen smaller than expected width, we take  size
-                    if (scope.width !== 'auto' && currentWidth > scope.width) {
-                        currentWidth = scope.width;
-                    }
+			var currentWidth;
+			var lastNumberColumns;
+			var columnsWidth;
 
-                    // This is use during resize, to detect a change of state with previous value
-                    var lastNumberColumns = scope.columns;
+			// This function calculate column width based on columns number and current width.
+			function calculate(columns, minWidth, callback) {
 
-                    // This function calculate column width based on columns number and current width.
-                    function calculate(callback) {
-                        var numberOfColumnPossible = parseInt(currentWidth / scope.columnsMinWidth);
+				console.log(columns, minWidth, currentWidth);
 
-                        if (lastNumberColumns !== numberOfColumnPossible) {
-                            lastNumberColumns = numberOfColumnPossible;
-                            // Case 1, we make them float
-                            if (numberOfColumnPossible < scope.columns) {
-                                scope.columnsWidth = (100 / numberOfColumnPossible) + '%';
-                            } else {
-                                scope.columnsWidth = (100 / scope.columns) + '%';
-                            }
-                        }
-                        if (callback !== undefined) {
-                            callback();
-                        }
-                    }
+				var numberOfColumnPossible = parseInt(currentWidth / minWidth);
 
-                    calculate();
+				if (lastNumberColumns !== numberOfColumnPossible) {
+					lastNumberColumns = numberOfColumnPossible;
+					// Case 1, we make them float
+					if (numberOfColumnPossible < columns) {
+						columnsWidth = (100 / numberOfColumnPossible) + '%';
+					} else {
+						columnsWidth = (100 / columns) + '%';
+					}
+				}
 
-                    // On each resize, we look if columns are smaller than scope.columnsMinWidth and
-                    // if it is we trigger a claculate and then a scope.apply()
-                    window.addEventListener('resize', function() {
-                        currentWidth = document.getElementById(scope.id).offsetWidth;
-                        calculate(function () {
-                            // scope.$apply() is required since scope is modified in an even.
-                            scope.$apply();
-                        });
-                    }, true);
+				console.log(numberOfColumnPossible);
+				if (callback !== undefined) {
+					callback();
+				}
+			}
 
-                }],
-                link: function(scope, element, attrs) {
-                    scope.dashboard = dashboardFactory.get(scope.id);
+			return {
+				restrict: 'E',
+				scope: {
+					'id': '@',
+					'width': '@',
+					'columns': '@',
+					'columnsMinWidth': '@'
+				},
+				templateUrl: 'src/dashboard.directive.html',
+				controller: ['$scope', function (scope) {
 
-                    scope.dashboard.set({
-                        'width': scope['width'],
-                        'columns': scope['columns'],
-                        'columnsMinWidth': scope['columnsMinWidth']
-                    });
+					currentWidth = $('#' + scope.id).parent().width();
 
-                    scope.grid = dashboardFactory.get(scope.id).grid;
+					// If screen smaller than expected width, we take size
+					if (scope.width !== 'auto' && currentWidth > scope.width) {
+						currentWidth = scope.width;
+					}
 
-                }
-            };
-        }]);
+					console.log(currentWidth);
+
+					calculate(scope.columns, scope.columnsMinWidth);
+
+					scope.columnsWidth = columnsWidth;
+
+					// This is use during resize, to detect a change of state with previous value
+					lastNumberColumns = scope.columns;
+
+					// On each resize, we look if columns are smaller than scope.columnsMinWidth and
+					// if it is we trigger a claculate and then a scope.apply()
+					window.addEventListener('resize', function() {
+						currentWidth = document.getElementById(scope.id).offsetWidth;
+						calculate(scope.columns, scope.columnsMinWidth, function () {
+							// scope.$apply() is required since scope is modified in an even.
+							scope.$apply();
+						});
+					}, true);
+
+				}],
+				link: function(scope, element, attrs) {
+
+					scope.dashboard = dashboardFactory.get(scope.id);
+
+					scope.dashboard.set({
+						'width': scope['width'],
+						'columns': scope['columns'],
+						'columnsMinWidth': scope['columnsMinWidth']
+					});
+
+					scope.grid = dashboardFactory.get(scope.id).grid;
+
+				}
+			};
+		}]);
 
 })();
--- src/dashboard.service.js
+++ src/dashboard.service.js
@@ -1,130 +1,149 @@
 (function() {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('dashboard')
-        .service('dashboardObject', DashboardObjectFunction);
+	angular
+		.module('dashboard')
+		.service('dashboardObject', DashboardObjectFunction);
 
-    DashboardObjectFunction.$inject = ['$injector'];
+	DashboardObjectFunction.$inject = ['$injector'];
 
-    /**
-     * @ngdoc service
-     * @module dashboard
-     * @name dashboardObject
-     * @description
-     *
-     * This is a dashboard object. Allow you to create an object.
-     *
-     */
-    function DashboardObjectFunction($injector) {
+	/**
+	 * @ngdoc service
+	 * @module dashboard
+	 * @name dashboardObject
+	 * @description
+	 *
+	 * This is a dashboard object. Allow you to create an object.
+	 *
+	 */
+	function DashboardObjectFunction($injector) {
 
-        // Return a function to be used as new User();
-        return function(params) {
-            var DEFAULT_DASHBOARD = {
-                id: null,
+		// Return a function to be used as new User();
+		return function(params) {
+			var DEFAULT_DASHBOARD = {
+				id: null,
 
-                nbComponent: 0,
+				nbComponent: 0,
 
-                isExtended: false,
+				isExtended: false,
 
-                grid: [],
+				grid: [],
 
-                // Here are stored option to manage our dashboard.
-                options: {
-                    // Full width of entiere dashboard
-                    'width': 'auto',
-                    // Number of columns in dashboard
-                    'columns': '2',
-                    // Min widht of columns.
-                    'columnsMinWidth': null
-                },
+				// Here are stored option to manage our dashboard.
+				options: {
+					// Full width of entiere dashboard
+					'width': 'auto',
+					// Number of columns in dashboard
+					'columns': '2',
+					// Min widht of columns.
+					'columnsMinWidth': null,
+					// Algorithm to define where to put component if no column
+					'algo': 'shorter'
+				},
 
-                /**
-                 * List of function to add
-                 */
-                add: add,
-                set: set,
-                saveAsString: saveAsString,
-                createFromString: createFromString
-            };
+				/**
+				 * List of function to add
+				 */
+				add: add,
+				set: set,
+				saveAsString: saveAsString,
+				createFromString: createFromString
+			};
 
-            var dashboardObject = DEFAULT_DASHBOARD;
+			var dashboardObject = DEFAULT_DASHBOARD;
 
-            return dashboardObject;
+			return dashboardObject;
 
-            /**
-             * Add a component in array
-             * @param {Object} component    Dashboard component
-             * @param {integer} column      Column number, starting at zero
-             */
-            function add(component, column) {
-                if (!column) {
-                    column = 0;
-                }
-                // create id to select easily
-                component.id = dashboardObject.id + '-' + dashboardObject.nbComponent;
-                dashboardObject.nbComponent++;
-                // If nocolumn yet, create one.
-                if (!dashboardObject.grid[column]) {
-                    dashboardObject.grid[column] = [];
-                }
-                // Add in column
-                dashboardObject.grid[column].push(component);
-            }
+			/**
+			 * Add a component in array
+			 * @param {Object} component    Dashboard component
+			 * @param {integer} column      Column number, starting at zero
+			 */
+			function add(component, column) {
+				if (!column) {
+					if (dashboardObject.options['algo'] === 'shorter') {
+						var i, shorterColumn = 0;
+						column = 0;
+						for (i = 1; i < dashboardObject.options['columns']; i++) {
+							if (dashboardObject.grid[i]) {
+								if (dashboardObject.grid[shorterColumn] &&
+									dashboardObject.grid[i].length < dashboardObject.grid[shorterColumn].length) {
+									column = i;
+									shorterColumn = i;
+								}
+							} else {
+								column = i;
+								shorterColumn = i;
+							}
+						}
+					} else {
+						column = 0;
+					}
+				}
+				// create id to select easily
+				component.id = dashboardObject.id + '-' + dashboardObject.nbComponent;
+				dashboardObject.nbComponent++;
+				// If nocolumn yet, create one.
+				if (!dashboardObject.grid[column]) {
+					dashboardObject.grid[column] = [];
+				}
+				// Add in column
+				dashboardObject.grid[column].push(component);
+			}
 
-            /**
-             * Set dashboard options.
-             */
-            function set(newOptions) {
-                // For each new option we override current one.
-                Object.keys(newOptions).forEach(function (key) {
-                    if (newOptions[key]) {
-                        dashboardObject.options[key] = newOptions[key];
-                    }
-                });
-            }
+			/**
+			 * Set dashboard options.
+			 */
+			function set(newOptions) {
+				// For each new option we override current one.
+				Object.keys(newOptions).forEach(function (key) {
+					if (newOptions[key]) {
+						dashboardObject.options[key] = newOptions[key];
+					}
+				});
+			}
 
-            /**
-             * Convert the dashboard to a String
-             * @return {String} Dashboard as a String
-             */
-            function saveAsString() {
-                var tmpColumns = [];
-                var tmpColumn = [];
+			/**
+			 * Convert the dashboard to a String
+			 * @return {String} Dashboard as a String
+			 */
+			function saveAsString() {
+				var tmpColumns = [];
+				var tmpColumn = [];
 
-                dashboardObject.grid.forEach(function(column) {
-                    tmpColumn = [];
-                    column.forEach(function(component) {
-                        tmpColumn.push({
-                            name: component.name,
-                            params: component.params
-                        });
-                    });
-                    tmpColumns.push(tmpColumn);
-                });
+				dashboardObject.grid.forEach(function(column) {
+					tmpColumn = [];
+					column.forEach(function(component) {
+						tmpColumn.push({
+							name: component.name,
+							params: component.params
+						});
+					});
+					tmpColumns.push(tmpColumn);
+				});
 
-                return JSON.stringify(tmpColumns);
-            }
+				return JSON.stringify(tmpColumns);
+			}
 
-            /**
-             * Create a dashboard from a String
-             * @param  {String} dashboardString Dashboard as a String
-             */
-            function createFromString(dashboardString) {
-                var tmpColumns = JSON.parse(dashboardString);
-                var nbColumns = 0;
+			/**
+			 * Create a dashboard from a String
+			 * @param  {String} dashboardString Dashboard as a String
+			 */
+			function createFromString(dashboardString) {
+				var tmpColumns = JSON.parse(dashboardString);
+				var nbColumns = 0;
 
-                tmpColumns.forEach(function(column) {
-                    column.forEach(function(component) {
-                        if ($injector.has(component.name)) {
-                            add(new $injector.get(component.name)(component.params), nbColumns);
-                        }
-                    });
-                    nbColumns++;
-                });
-            }
+				tmpColumns.forEach(function(column) {
+					column.forEach(function(component) {
+						if ($injector.has(component.name)) {
+							add(new $injector.get(component.name)(component.params), nbColumns);
+						}
+					});
+					nbColumns++;
+				});
+			}
 
-        };
+		};
 
-    }
+	}
 })();
