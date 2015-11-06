642a4a1cde22780b9fb274aafe142ad7e8418fdd
mambax <dmnkmyr@gmail.com>
2015-11-06 15:48:57 +0100

    Edit travis.yml, gulpfile.js and deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,7 @@ if [ "$github_user" == "fluanceit" ] || [ "$github_user" == "mambax" ]; then
     git commit -m "Deploy to GitHub Pages"
     git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
     echo "*** Deployed docs ***"
+    cd ..
 	echo "*** Deploying dist ***"
 	git clone https://${GH_TOKEN}@github.com/fluanceit/bower-angular-dashboard.git
 	cp -rf ./dist ./bower-angular-dashboard
