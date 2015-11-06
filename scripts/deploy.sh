120ebda1491996d0b78405c65b3fcfe31bc6630c
mambax <dmnkmyr@gmail.com>
2015-11-06 15:22:26 +0100

    Edit travis.yml, gulpfile.js and deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,8 @@ github_user=`git remote -v | grep push | sed -e 's/^.*github.com\///g' -e 's/\/.
 echo "Github user: " $github_user
 if [ "$github_user" == "fluanceit" ] || [ "$github_user" == "mambax" ]; then
 	echo "*** Deploying docs ***"
-    cd angular-dashboard/build_docs
+	ls
+    cd build_docs
     git init
     git config user.name "Travis CI"
     git config user.email "frontdev@fluance.net"
@@ -13,7 +14,9 @@ if [ "$github_user" == "fluanceit" ] || [ "$github_user" == "mambax" ]; then
     git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
     echo "*** Deployed docs ***"
 	echo "*** Deploying dist ***"
+	ls
 	cd ..
+	ls
 	git clone https://${GH_TOKEN}@github.com/fluanceit/bower-angular-dashboard.git
 	cp -rf ./dist ./bower-angular-dashboard
 	cd bower-angular-dashboard
