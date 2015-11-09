9bb0e638243767df6fee925aae0bea1297aad66f
mambax <dmnkmyr@gmail.com>
2015-11-09 14:17:01 +0100

    Use commit message from source repo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,6 +13,12 @@ if [ "$github_user" == "fluanceit" ] || [ "$github_user" == "mambax" ]; then
     git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
     echo "*** Deployed docs ***"
     cd ..
+    echo "*** Fetching commit message from source repo ***"
+    git clone https://github.com/fluanceit/angular-dashboard.git
+    cd angular-dashboard
+    tag_comment=`git log -1 --pretty=%B`
+    cd ..
+    echo "*** Got commit message ***"
 	echo "*** Deploying dist ***"
 	git clone https://github.com/fluanceit/bower-angular-dashboard.git
 	cp -rf ./dist/* ./bower-angular-dashboard
@@ -22,11 +28,11 @@ if [ "$github_user" == "fluanceit" ] || [ "$github_user" == "mambax" ]; then
     git config user.email "frontdev@fluance.net"
     git config push.default simple
     git add .
-    git commit -m "Deploy to GitHub Dist Repo"
+    git commit -m "$tag_comment"
     tag_sha=`git log --pretty=format:'%h' -n 1`
     tag_ver=`cat bower.json | grep -Po '(?<="version": ")[^"]*'`
     git tag "v$tag_ver-build.${TRAVIS_BUILD_NUMBER}+sha.$tag_sha"
     git push -f -q --tags "https://$github_user:$GITHUB_API_KEY@${GH_BOWER_REF}" master > /dev/null 2>&1
 	echo "*** Deployed dist ***"
 fi
-echo "*** done ***"
+echo "*** done deploy.sh ***"
