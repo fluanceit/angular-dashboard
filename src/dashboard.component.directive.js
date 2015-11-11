529eb5dc66e4538eee9ae61f25befced3531aec0
Sebastien BARBIER <contact@sebastienbarbier.com>
2015-11-11 17:19:30 +0100

    Implement controller in component.

--- src/dashboard.component.directive.js
+++ src/dashboard.component.directive.js
@@ -21,26 +21,45 @@
                         angular.extend(scope, scope.component.scope);
                     }
 
+                    scope.component.states.default.controller();
+
                     scope.openExtended = function() {
                         if (!scope.dashboard.isStateSorting) {
                             scope.dashboard.isExtended = true;
                             scope.component.isExtended = true;
+                            // Execute JS
+                            if (scope.component.states.extended && scope.component.states.extended.controller) {
+                                scope.component.states.extended.controller();
+                            }
+
                         }
                     };
                     scope.closeExtended = function() {
                         if (!scope.dashboard.isStateSorting) {
                             scope.dashboard.isExtended = false;
                             scope.component.isExtended = false;
+                            if (scope.component.states.default && scope.component.states.default.controller) {
+                                scope.component.states.default.controller();
+                            }
+
                         }
                     };
                     scope.openSettings = function() {
                         if (!scope.dashboard.isStateSorting) {
                             scope.component.displaySettings = true;
+                            if (scope.component.states.settings && scope.component.states.settings.controller) {
+                                scope.component.states.settings.controller();
+                            }
+
                         }
                     };
                     scope.closeSettings = function() {
                         if (!scope.dashboard.isStateSorting) {
                             scope.component.displaySettings = false;
+                            if (scope.component.states.default && scope.component.states.default.controller) {
+                                scope.component.states.default.controller();
+                            }
+
                         }
                     };
                 }
